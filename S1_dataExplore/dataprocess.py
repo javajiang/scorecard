fa1aa76e49b0c4fc649dd034e9e98837944ed431
changandao <wangshen689@sina.com>
2016-03-12 15:16:37 +0800

    new stories
    new

--- S1_dataExplore/dataprocess.py
+++ S1_dataExplore/dataprocess.py
@@ -6,36 +6,58 @@ __date__   = "2016.3.8"
 import random
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 from datetime import datetime
-
 
 FilePath = '../data/'
 Dataset = 'Training Set/'
 MasterFile = FilePath + Dataset + 'PPD_Training_Master_GBK_3_1_Training_Set.csv'
-fhadle = open(MasterFile,'r')
-data = fhadle.readlines()
-fhadle.close()
-data = map(lambda x:x.decode('gbk').strip().split(',')+['train_master'],data)
-MasterData = pd.DataFrame(data[1:],columns=data[0][:-1]+['flag'])
+Master = pd.read_csv(MasterFile,encoding="gb18030")
+TypeStatement = pd.read_csv(FilePath + 'TypeState.csv')
 
-'''Summury = pd.read_csv(FilePath + 'PPD_summary.csv')
-print len(Summury)
-se = Summury.loc[:,'Columns':'type']
-for row in se.itertuples():
-    print row[1],row[2]
-    print MasterData[row[1]]
-    if row[1] == 'ListingInfo':
-        pd.to_datetime(MasterData[row[1]])
-    elif row[1] == 'UserInfo_1' or row[1] == 'UserInfo_10':
-        pass
-    else:
-        MasterData[row[1]] = MasterData[row[1]].astype(row[2])
-print MasterData.dtypes
-#Describe.to_csv('../Output/foo.csv', encoding='gbk')
-#raw_input()'''
+###### handeling the dtype ######
+TypeStatement = TypeStatement.dropna(axis=1)
+se = TypeStatement.set_index('Idx')
+ser = pd.Series(se['Index'],index = TypeStatement['Idx'])
+for i in ser.index:
+    if ser[i]=='Categorical':
+        ser[i] = 'object'
+    #else:pass
+    elif ser[i]=='Numerical':
+        if Master[i].dtypes == 'object':
+            ser[i] = 'float64'
+        else:pass
+    else:pass
+    try:
+        Master[i] = Master[i].astype(ser[i])
+    except:pass
+Master.dtypes.to_csv('../Output/S1/newtypes.csv')
 
-Summury = pd.read_csv(FilePath + 'PPD_summary.csv')
-Summary = Summury.ix[:,'Columns':'type']
+###### dataAnalyse #######
+Mfeature = Master.describe()
+def generatedct(serie,dct):
+    for s in serie:
+        dct[s] = dct.get(s,0)+1
+    return dct
+
+Mlist = []
+for clm in Master.columns:
+    Mdct = {}
+    Mdct = generatedct(Master[clm],Mdct)
+    Mlist.append(Mdct)
+
+#Mfile = open('../Output/S1/tem.txt')
+Xxis=Mlist[3].keys()
+Yxis=Mlist[3].values()
+
+plt.bar(Xxis, Yxis)
+plt.show()
+
+###### dataCleaning #######
+def WOEFunc(A,B,k):
+
+
+'''Summary = Summury.ix[:,'Columns':'type']
 se = Summary.set_index('Columns')
 se1 = pd.Series(se['type'],index = se.index)
 
@@ -43,8 +65,14 @@ for i in se1.index:
     try:
         MasterData[i] = MasterData[i].astype(se1.ix[i])
     except:pass
-    
+MasterData1 = MasterData.replace(-1,np.nan)
+print MasterData1['WeblogInfo_3'].dtypes
+
+#print MasterData1
+#print MasterData1.isnull()'''
+'''
 print MasterData.dtypes
 Types = MasterData.dtypes
 Types.to_csv("../Output/S1/sel.csv",index=False,encoding="gb18030")
 Types.to_csv('../Output/S1/types.csv')
+'''

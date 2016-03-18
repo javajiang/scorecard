9d52d07e75166151c95154849bbbff247e199c90
SEAN <wangshen689@sina.com>
2016-03-18 10:38:53 +0800

    NEWLY

--- S1_dataExplore/IV.py
+++ S1_dataExplore/IV.py
@@ -4,8 +4,9 @@ __author__ = "changandao&jiangweiwu"
 __date__   = "2016.3.8"
 
 import numpy as np
-import math
 import pandas as pd
+#from sklearn import *
+#from sklearn.preprocessing import Imputer
 import matplotlib.pyplot as plt
 from datetime import datetime
 
@@ -40,7 +41,6 @@ def IVCAL(good,bad):
     IV = ivser.sum()
     return IV
 
-<<<<<<< HEAD
 
 def resetType(df,oridf):
     typetmp = df.dropna(axis=1)
@@ -51,7 +51,7 @@ def resetType(df,oridf):
             ser[i] = 'object'
         elif ser[i]=='Numerical':
             if oridf[i].dtypes == 'object':
-                print i, oridf.columns.get_loc(i)
+                #print i, oridf.columns.get_loc(i)
                 ser[i] = 'float64'
             else:pass
         else:pass
@@ -62,21 +62,33 @@ def resetType(df,oridf):
 
 
 def Objectprocess(goodser, badser,totalser):
+
     goodOValue = goodser.value_counts()
     badOValue = badser.value_counts()
     totalOvalue = totalser.value_counts()
-    badtmp = goodOValue.copy()
-    goodtmp = badOValue.copy()
+    #print totalOvalue,goodOValue,badOValue
+    badtmp = totalOvalue.copy()
+    goodtmp = totalOvalue.copy()
     badtmp[(totalOvalue - badOValue).notnull()] = badOValue+1
     badtmp[(totalOvalue - badOValue).isnull()] = 1
     goodtmp[(totalOvalue - goodOValue).notnull()] = goodOValue+1
     goodtmp[(totalOvalue - goodOValue).isnull()] = 1
-
     goodtmp = goodtmp.sort_index()
     badtmp = badtmp.sort_index()
-
+    good_pct = goodtmp/goodtmp.sum()
+    bad_pct = badtmp/badtmp.sum()
+    totalOvalue = totalOvalue.sort_index()
+    total =totalOvalue.copy()
+    Woe_of_O = WOEFUNC(good_pct,bad_pct)
+    total[:] = Woe_of_O
+    tmptotal = totalser.copy()
+    for j in totalser.isnull():
+        if j:
+            print 'wow!'
+    #for i in totalser.index:
+    #    print total[totalser[i]]
     IV_Objekt = IVCAL(goodtmp,badtmp)
-    return IV_Objekt
+    return IV_Objekt,tmptotal
 
 
 def NUMprocess(goodser, badser, totalser,bin):
@@ -94,98 +106,67 @@ def NUMprocess(goodser, badser, totalser,bin):
     badsum = badsum.sort_index()# sort the index
     badsum[(totalsum==0) != (badsum == 0)] = 1
     goodsum[(totalsum==0) != (goodsum == 0)] = 1
-
     IV_NUM = IVCAL(goodsum,badsum)
     return IV_NUM
 
 
-#def replaceObject():
-
+def replace(ser,WOE):
+    tmpser = ser.copy()
+    tmpser = tmpser.value_counts()
+    tmpser = tmpser.sort_index()
+    tmpser[:] = WOE
+    return tmpser
+    #tmpser = tmpser.reset
 
 def IVFunc(Aframe, k):
-    if -1 in Aframe:
-        print 'wow!'
-=======
-def IVFunc(Aframe, k): #k is num of segments for continue variable
->>>>>>> origin/master
     lst = []
     final_IV = 0
     count = 0
     dicretcount = 0
     newFrame = Aframe.set_index('Idx')
-<<<<<<< HEAD
-=======
-    if -1 in newFrame:
-        print 'aaaaaa!'
->>>>>>> origin/master
     goodN = newFrame.groupby('target').get_group(1)
     badN = newFrame.groupby('target').get_group(0)
 
-    dropgroup = []
-   
+    dropgroup =[]
     for clm in newFrame.columns:
         if len(newFrame[clm].dropna())<30000*0.8:
-<<<<<<< HEAD
             #print 'column need to be deleted ',clm,newFrame.columns.get_loc(clm)
             del newFrame[clm]
             continue
-=======
-            print 'column need to be deleted ',clm,newFrame.columns.get_loc(clm)
-            #del newFrame[clm]
-            #raw_input()
->>>>>>> origin/master
+
         else:
+            newFrame[clm] = newFrame[clm].interpolate()
             #oricount = newFrame[clm].count() #total count without NaN
             #totalTypes = len(newFrame[clm].value_counts())
-            ###### Handling the Categories ######
-            oricount = newFrame[clm].count() #total count without NaN
-            totalTypes = len(newFrame[clm].value_counts())
-            #print oricount
-            #raw_input()
-            #del newFrame[clm]
-            continue
+
             ###### Handling the Categories ######
             if newFrame[clm].dtypes =='object':
-<<<<<<< HEAD
-                #print 'oblect',clm, newFrame.columns.get_loc(clm)
-                final_IV = Objectprocess(goodN[clm],badN[clm],newFrame[clm])
+                #newFrame.ix[newFrame[clm].isnull(),clm] = u'no'
+                #goodN.ix[goodN[clm].isnull(),clm] = u'no'
+                #badN.ix[badN[clm].isnull(),clm] = u'no'
+
+                print 'oblect',clm, newFrame.columns.get_loc(clm)
+                final_IV,newFrame.ix[clm] = Objectprocess(goodN[clm],badN[clm],newFrame[clm])
+                #print newFrame[clm]
                 if final_IV >1:
                     count+=1
-                    print 'BUG'
+                    #print 'BUG'
                     continue
-=======
-                goodOValue = goodN[clm].value_counts()
-                badOValue = badN[clm].value_counts()
-                totalOvalue = newFrame[clm].value_counts()
-                #print len(goodOValue),len(badOValue),len(totalOvalue)
-                #raw_input()
-                badtmp = totalOvalue.copy()
-                goodtmp = totalOvalue.copy()
-                badtmp[(totalOvalue - badOValue).notnull()] = badOValue #need to add 1 also
-                badtmp[(totalOvalue - badOValue).isnull()] = 1
-                goodtmp[(totalOvalue - goodOValue).notnull()] = goodOValue
-                goodtmp[(totalOvalue - goodOValue).isnull()] = 1
 
-                IV = IVCAL(goodtmp,badtmp)
-
->>>>>>> origin/master
             ######## numerical#######
             else:
+                #imp = preprocessing.Imputer(missing_values='NaN', strategy='mean', axis=0)
+                #imp.fit(newFrame[clm])
+                #newFrame[clm]=imp.transform(newFrame[clm])
                 ####### Discretization
                 Maxmum = float(newFrame[clm].max())
                 Minmum = float(newFrame[clm].min())
-                print Maxmum,Minmum
                 dist = float((Maxmum - Minmum)/k)
-<<<<<<< HEAD
-                #print Maxmum, Minmum
                 if Maxmum < 100:
-                    final_IV = Objectprocess(goodN[clm],badN[clm],newFrame[clm])
+                    final_IV,newFrame.ix[:,clm] = Objectprocess(goodN[clm],badN[clm],newFrame[clm])
                     dicretcount+=1
-                    print 'discret: ', final_IV
-                    #print 'depends: ', clm, newFrame.columns.get_loc(clm)
                 else:
                     if dist == 0:
-                        #print 'column need to be deleted ',clm,newFrame.columns.get_loc(clm)
                         dropgroup.append(newFrame[clm])
                         #count +=1
                         del newFrame[clm]
@@ -197,70 +178,22 @@ def IVFunc(Aframe, k): #k is num of segments for continue variable
                     final_IV = NUMprocess(goodN[clm],badN[clm],newFrame[clm],bins)
                     if final_IV >1:
                         count+=1
-                        #print final_IV
                         continue
                     elif final_IV >0.1:
                         pass
 
             lst.append(final_IV)
-    print dicretcount,count
+    #print dicretcount,count
     return lst,newFrame
-=======
-                if dist == 0.:
-                    print 'column need to be deleted ',clm,newFrame.columns.get_loc(clm)
-                    #dropgroup.append(newFrame[clm])
-                    #del newFrame[clm]
-                if dist == 0:
-                    dropgroup.append(newFrame[clm])
-                    count +=1
-                    print 'haha'
-                    del newFrame[clm]
 
-                    continue
-                bins =[]
-                for i in range(0,k+1):
-                    start = Minmum + i*dist
-                    bins.append(start)
-                totalcuts = pd.cut(newFrame[clm], bins, right = True, include_lowest = True)
-                goodcuts = pd.cut(goodN[clm], bins, right = True, include_lowest = True)
-                badcuts = pd.cut(badN[clm], bins, right = True, include_lowest = True)
 
-                ####### do statistics
-                totalsum = totalcuts.value_counts()
-                goodsum = goodcuts.value_counts()
-                badsum = badcuts.value_counts()
-
-                totalsum = totalsum.sort_index() # sort the index
-                goodsum = goodsum.sort_index()# sort the index
-                badsum = badsum.sort_index()# sort the index
-                badsum[(totalsum==0) != (badsum == 0)] = 1
-                goodsum[(totalsum==0) != (goodsum == 0)] = 1
-                IV = IVCAL(goodsum,badsum)
-                if IV >1:
-                    #print clm
-                    count+=1
-                    continue
-                elif IV >0.1:
-                    print 'this is good: ' + clm
-            lst.append(IV)
-    return lst,newFrame,count
->>>>>>> origin/master
-
+#imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
 Master = Master.replace(-1, np.nan)
 Master = resetType(TypeStatement,Master)
 IV,DF = IVFunc(Master,20)
 #print DF
-print IV
-print len(DF.columns)
-<<<<<<< HEAD
+#print IV
+#print len(DF.columns)
 '''for element in IV:
     if element > 1:
         print element'''
-=======
-for element in IV:
-    if element < 1:
-        print element
-#print IV
-#print len(DF.columns)
-#DF.to_csv('../Output/S1/staticstics/firstclean.csv',encoding = 'gb18030')
->>>>>>> origin/master

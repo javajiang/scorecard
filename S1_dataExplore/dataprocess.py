6113e44997ba4733ea18893da3968ed91643c98b
weiwujiang <wwjiang.ustc@gmail.com>
2016-03-19 22:29:27 +0800

    revise for preprocess

--- S1_dataExplore/dataprocess.py
+++ S1_dataExplore/dataprocess.py
@@ -12,12 +12,10 @@ FilePath = '../data/'
 Dataset = 'Training Set/'
 MasterFile = FilePath + Dataset + 'PPD_Training_Master_GBK_3_1_Training_Set.csv'
 Master = pd.read_csv(MasterFile,encoding="gb18030")
-
 TypeStatement = pd.read_csv(FilePath + 'TypeState.csv')
+Master.dtypes.to_csv('../Output/S1/statistics/oritypes.csv')
 
-Master.dtypes.to_csv('../Output/S1//Test/oritypes.csv')
-
-print Master.info()
+#print Master.info()
 
 ###### handeling the dtype ######
 TypeStatement = TypeStatement.dropna(axis=1)
@@ -36,46 +34,57 @@ for i in ser.index:
     try:
         Master[i] = Master[i].astype(ser[i])
     except:pass
-Master.dtypes.to_csv('../Output/S1/newtypes.csv')
-'''
+Master.dtypes.to_csv('../Output/S1/statistics/newtypes.csv')
+Master_describe = Master.describe().transpose().to_csv('../Output/S1/statistics/summary/describe_summary.csv',encoding='utf-8',na_rep='NaN')
+
+
+
 Mdct = {}
-count = 0
 for clm in Master.columns:
+    if clm=='Idx':
+        continue
     num = Master[clm].value_counts()
-    #key = (clm, count)
-    Mdct[count] = num
-    count += 1
+    num.to_csv('../Output/S1/statistics/summary/'+clm+'_count.csv',encoding='utf-8',na_rep='NaN')
+    Mdct[clm] = num
 
-for i in range(1,len(Mdct.keys())):
+print len(Mdct)
+
+
+
+for key in Mdct.keys():
+    if key=='Idx':
+        continue
+    #print Mdct[key].index
     try:
-        fig = plt.figure()
-        plt.bar(Mdct[i].index, Mdct[i])
-        fig.savefig('../Output/S1//staticstics'+str(i)+'.png', format='png')
-        plt.close(fig)
+        if len(Mdct[key])>100:
+               print key,'too many items',str(len(Mdct[key]))
+               continue
+        plt.bar(left=Mdct[key].index,height=Mdct[key])
+        plt.savefig('../Output/S1/statistics/fig/'+str(key)+'.png', format='png')
+        plt.close('all')
     except:
-        print i
-'''
+        print key,'fail'
 
 
+##
+##
+##Summary = Summury.ix[:,'Columns':'type']
+##se = Summary.set_index('Columns')
+##se1 = pd.Series(se['type'],index = se.index)
+##
+##for i in se1.index:
+##    try:
+##        MasterData[i] = MasterData[i].astype(se1.ix[i])
+##    except:pass
+##MasterData1 = MasterData.replace(-1,np.nan)
+##print MasterData1['WeblogInfo_3'].dtypes
+##
+###print MasterData1
+###print MasterData1.isnull()
+##
+##print MasterData.dtypes
+##Types = MasterData.dtypes
+##Types.to_csv("../Output/S1/sel.csv",index=False,encoding="gb18030")
+##Types.to_csv('../Output/S1/types.csv')
 
-'''Summary = Summury.ix[:,'Columns':'type']
-se = Summary.set_index('Columns')
-se1 = pd.Series(se['type'],index = se.index)
-
-for i in se1.index:
-    try:
-        MasterData[i] = MasterData[i].astype(se1.ix[i])
-    except:pass
-MasterData1 = MasterData.replace(-1,np.nan)
-print MasterData1['WeblogInfo_3'].dtypes
-
-#print MasterData1
-#print MasterData1.isnull()'''
-'''
-print MasterData.dtypes
-Types = MasterData.dtypes
-Types.to_csv("../Output/S1/sel.csv",index=False,encoding="gb18030")
-Types.to_csv('../Output/S1/types.csv')
-<<<<<<< Updated upstream
-'''
 

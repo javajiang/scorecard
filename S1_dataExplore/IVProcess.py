b9e4ce30665de320704a8f7415eb23175bfcfe76
weiwujiang <wwjiang.ustc@gmail.com>
2016-03-25 08:38:10 +0800

    revise parameter

--- S1_dataExplore/IVProcess.py
+++ S1_dataExplore/IVProcess.py
@@ -269,7 +269,7 @@ if __name__=='__main__':
     MasterTestile = FilePath + Testset + 'PPD_Master_GBK_2_Test_Set.csv'
     MasterFileType = FilePath + 'TypeState.csv'
 
-    ivpro = IVProcess(MasterTrainFile,MasterTestile,MasterFileType,20,1,0,0.75)
+    ivpro = IVProcess(MasterTrainFile,MasterTestile,MasterFileType,20,1,0,0.7)
     IV, DF, droped = ivpro.IVFunc()
     
     #imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
